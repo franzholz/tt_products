6b1d5e78b1da7ac00ae1a1f49d40230b1a626beb
Benjamin Pick <b.pick@yellowtree.de>
2018-10-16 15:07:16 +0200

    Implement tceSingleOrder

--- hooks/class.tx_ttproducts_hooks_be.php
+++ hooks/class.tx_ttproducts_hooks_be.php
@@ -71,6 +71,7 @@ class tx_ttproducts_hooks_be implements t3lib_Singleton {
 			// Field configuration from TCA:
 		$config = $PA['fieldConf']['config'];
 		$orderData = unserialize($row['orderData']);
+
 		if (
 			is_array($orderData) &&
 			isset($orderData['html_output']) &&
@@ -83,6 +84,58 @@ class tx_ttproducts_hooks_be implements t3lib_Singleton {
 		}
 
 		return $result;
+	}
+	
+	public function tceSingleOrder($PA, $fobj) {
+	    $ret = 'Keine Produkte bestellt.';
+	    
+	    $table = $PA['table'];
+	    $field = $PA['field'];
+	    $row   = $PA['row'];
+
+	    $config = $PA['fieldConf']['config'];
+	    $orderData = unserialize($row['orderData']);
+	    
+	    // Typoscript is not set in backend. Hard-coding this for now.
+	    $cnf = t3lib_div::makeInstance('tx_ttproducts_config');
+	    $cnf->conf['priceDec'] = 2;
+	    $cnf->conf['priceDecPoint'] = ',';
+	    $cnf->conf['priceThousandPoint'] = '.';
+	    
+	    $priceViewObj = t3lib_div::makeInstance('tx_ttproducts_field_price_view');
+	    
+	    
+	    if (
+	        is_array($orderData) && 
+	        isset($orderData['itemArray']) &&
+	        isset($orderData['calculatedArray'])
+	        ) {
+	            $array = $orderData['calculatedArray'];
+	            $count = $array['count'];
+	            $shipping = $priceViewObj->priceFormat($array['priceTax']['shipping']);
+	            $total = $priceViewObj->priceFormat($array['priceTax']['total']);
+	            
+	            $data = reset($orderData['itemArray']);
+	            $data = reset($data);
+	            if (is_array($data) && count($data) > 0) {
+	                $ret = $count . " Artikel bestellt:";
+	                $ret .="<ul>";
+	                foreach($data as $row) {
+	                    $count = $row['count'];
+	                    $price = $priceViewObj->priceFormat($row['priceTax']);
+	                    $title = $row['rec']['title'];
+	                    
+                        $ret .= "<li>$count x $title ($price)</li>";
+	                }
+	                $ret .= "</ul>";
+	                
+	                $ret .= "Gesamt: $total<br>(inkl. $shipping Versandkosten)";
+	                
+	            }
+	        }
+	        
+	    
+	    return $ret;
 	}
 }
 
