6ce14583be0d6c2dd8289f8786736130fb931218
Franz Holzinger <holzinger@ttproducts.de>
2021-02-17 17:35:23 +0100

    * compatibility TYPO3 10.4: new TypoScript conditions with Symfony expressions

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -3,10 +3,16 @@ defined('TYPO3_MODE') || die('Access denied.');
 
 call_user_func(function () {
 
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile( TT_PRODUCTS_EXT, 'Configuration/TypoScript/PluginSetup/Main/', 'Shop System');
+   if (
+            version_compare(TYPO3_version, '9.5.0', '<')
+    ) {
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile( TT_PRODUCTS_EXT, 'Configuration/TypoScript/PluginSetup/Main/', 'Shop System');
+    } else {
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile( TT_PRODUCTS_EXT, 'Configuration/TypoScript/PluginSetup/Compatibility8.7/Main/', 'Shop System');
+    }
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(TT_PRODUCTS_EXT, 'Configuration/TypoScript/PluginSetup/Int/',  'Shop System Variable Content');
-
     if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded( 'searchbox')) {
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(TT_PRODUCTS_EXT, 'Configuration/TypoScript/PluginSetup/Searchbox/', 'Shop System Search Box');
     }
+
 });
