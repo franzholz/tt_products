b1e52ec93b4d4db7eed346487a143d1a74b146a9
Franz Holzinger <holzinger@ttproducts.de>
2021-03-05 23:50:50 +0100

    readd the $assetCollector->addInlineJavaScript as a replacement for $GLOBALS[TSFE]->setJS($JSfieldname, $code);

--- control/class.tx_ttproducts_javascript.php
+++ control/class.tx_ttproducts_javascript.php
@@ -511,8 +511,8 @@ if (!Array.prototype.indexOf) { // published by developer.mozilla.org
 		} // switch
 
 
-		if (!$bError)	{
-			if ($code)	{
+        if (!$bError)	{
+            if ($code)	{
 				if ($bDirectHTML)	{
                     if (
                         version_compare(TYPO3_version, '9.5.0', '>=')
@@ -523,7 +523,12 @@ if (!Array.prototype.indexOf) { // published by developer.mozilla.org
                         $GLOBALS['TSFE']->additionalHeaderData[$JSfieldname] = $code;
                     }
 				} else {
-                    $GLOBALS['TSFE']->setJS($JSfieldname, $code);
+                    if (version_compare(TYPO3_version, '10.4.0', '>=')) {
+                        $assetCollector = GeneralUtility::makeInstance(\TYPO3\CMS\Core\Page\AssetCollector::class);
+                        $assetCollector->addInlineJavaScript($JSfieldname, $code, [], ['priority' => true]);
+                    } else {
+                        $GLOBALS['TSFE']->setJS($JSfieldname, $code);
+                    }
 				}
 			}
 		}
