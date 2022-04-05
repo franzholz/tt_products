7d9aea36dad034b00318c2521979a07abfb035c8
Franz Holzinger <franz.holzinger@festwein.de>
2022-04-05 22:16:49 +0200

    no clickIntoSubmenu available in this version

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,51 +1,54 @@
 <?php
 defined('TYPO3_MODE') || die('Access denied.');
 
-$table = 'tt_content';
+call_user_func(function () {
 
-$GLOBALS['TCA'][$table]['types']['list']['subtypes_excludelist']['5'] = 'layout,select_key';
-$GLOBALS['TCA'][$table]['types']['list']['subtypes_addlist']['5'] = 'pi_flexform';
+    $table = 'tt_content';
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('5', 'FILE:EXT:' . TT_PRODUCTS_EXT . '/pi1/flexform_ds_pi1.xml');
+    $GLOBALS['TCA'][$table]['types']['list']['subtypes_excludelist']['5'] = 'layout,select_key';
+    $GLOBALS['TCA'][$table]['types']['list']['subtypes_addlist']['5'] = 'pi_flexform';
 
-if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('searchbox')) {
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('5', 'FILE:EXT:' . TT_PRODUCTS_EXT . '/pi1/flexform_ds_pi1.xml');
 
-    $listType = TT_PRODUCTS_EXT . '_pi_search';
+    if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('searchbox')) {
+
+        $listType = TT_PRODUCTS_EXT . '_pi_search';
+        $GLOBALS['TCA'][$table]['types']['list']['subtypes_excludelist'][$listType] = 'layout,select_key';
+        $GLOBALS['TCA'][$table]['types']['list']['subtypes_addlist'][$listType] = 'pi_flexform';
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($listType, 'FILE:EXT:' . TT_PRODUCTS_EXT . '/pi_search/flexform_ds_pi_search.xml');
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
+            [
+                'LLL:EXT:' . TT_PRODUCTS_EXT . DIV2007_LANGUAGE_SUBPATH .
+                'PiSearch/locallang_db.xlf:tt_content.list_type_pi_search',
+                $listType
+            ],
+            'list_type',
+            TT_PRODUCTS_EXT
+        );
+    }
+
+    $listType = TT_PRODUCTS_EXT . '_pi_int';
     $GLOBALS['TCA'][$table]['types']['list']['subtypes_excludelist'][$listType] = 'layout,select_key';
     $GLOBALS['TCA'][$table]['types']['list']['subtypes_addlist'][$listType] = 'pi_flexform';
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($listType, 'FILE:EXT:' . TT_PRODUCTS_EXT . '/pi_search/flexform_ds_pi_search.xml');
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($listType, 'FILE:EXT:' . TT_PRODUCTS_EXT . '/pi_int/flexform_ds_pi_int.xml');
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
-        array('LLL:EXT:' . TT_PRODUCTS_EXT . DIV2007_LANGUAGE_SUBPATH .
-            '/PiSearch/locallang_db.xlf:tt_content.list_type_pi_search',
+        [
+            'LLL:EXT:' . TT_PRODUCTS_EXT . DIV2007_LANGUAGE_SUBPATH .
+            'PiInt/locallang_db.xlf:tt_content.list_type_pi_int',
             $listType
-        ),
+        ],
         'list_type',
         TT_PRODUCTS_EXT
     );
-}
 
-$listType = TT_PRODUCTS_EXT . '_pi_int';
-$GLOBALS['TCA'][$table]['types']['list']['subtypes_excludelist'][$listType] = 'layout,select_key';
-$GLOBALS['TCA'][$table]['types']['list']['subtypes_addlist'][$listType] = 'pi_flexform';
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($listType, 'FILE:EXT:' . TT_PRODUCTS_EXT . '/pi_int/flexform_ds_pi_int.xml');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
-    array(
-        'LLL:EXT:' . TT_PRODUCTS_EXT . DIV2007_LANGUAGE_SUBPATH .
-        '/PiInt/locallang_db.xlf:tt_content.list_type_pi_int',
-        $listType
-    ),
-    'list_type',
-    TT_PRODUCTS_EXT
-);
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
-    array(
-        'LLL:EXT:' . TT_PRODUCTS_EXT . DIV2007_LANGUAGE_SUBPATH .
-        '/locallang_db.xlf:tt_content.list_type_pi1',
-        '5'
-    ),
-    'list_type',
-    TT_PRODUCTS_EXT
-);
-
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
+        [
+            'LLL:EXT:' . TT_PRODUCTS_EXT . DIV2007_LANGUAGE_SUBPATH .
+            'locallang_db.xlf:tt_content.list_type_pi1',
+            '5'
+        ],
+        'list_type',
+        TT_PRODUCTS_EXT
+    );
+});
 
