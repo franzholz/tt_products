f085ece43ec1ef7c6970c92432cc9c75f06e05ce
Franz Holzinger <franz.holzinger@festwein.de>
2022-09-09 19:37:30 +0200

    * compatibility TYPO3 8.7: deprecation no 77934: The field `select_key` of the table `tt_content` is not used in the core and has been removed.

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -5,7 +5,7 @@ call_user_func(function () {
 
     $table = 'tt_content';
 
-    $GLOBALS['TCA'][$table]['types']['list']['subtypes_excludelist']['5'] = 'layout,select_key';
+    $GLOBALS['TCA'][$table]['types']['list']['subtypes_excludelist']['5'] = 'layout';
     $GLOBALS['TCA'][$table]['types']['list']['subtypes_addlist']['5'] = 'pi_flexform';
 
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('5', 'FILE:EXT:' . TT_PRODUCTS_EXT . '/pi1/flexform_ds_pi1.xml');
@@ -13,7 +13,7 @@ call_user_func(function () {
     if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('searchbox')) {
 
         $listType = TT_PRODUCTS_EXT . '_pi_search';
-        $GLOBALS['TCA'][$table]['types']['list']['subtypes_excludelist'][$listType] = 'layout,select_key';
+        $GLOBALS['TCA'][$table]['types']['list']['subtypes_excludelist'][$listType] = 'layout';
         $GLOBALS['TCA'][$table]['types']['list']['subtypes_addlist'][$listType] = 'pi_flexform';
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($listType, 'FILE:EXT:' . TT_PRODUCTS_EXT . '/pi_search/flexform_ds_pi_search.xml');
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
@@ -28,7 +28,7 @@ call_user_func(function () {
     }
 
     $listType = TT_PRODUCTS_EXT . '_pi_int';
-    $GLOBALS['TCA'][$table]['types']['list']['subtypes_excludelist'][$listType] = 'layout,select_key';
+    $GLOBALS['TCA'][$table]['types']['list']['subtypes_excludelist'][$listType] = 'layout';
     $GLOBALS['TCA'][$table]['types']['list']['subtypes_addlist'][$listType] = 'pi_flexform';
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($listType, 'FILE:EXT:' . TT_PRODUCTS_EXT . '/pi_int/flexform_ds_pi_int.xml');
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
