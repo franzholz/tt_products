f1e72f95e63de47dc8549253170fe6c9564c35f6
Franz Holzinger <franz.holzinger@festwein.de>
2024-06-25 11:40:02 +0200

    fix #268 again

--- control/class.tx_ttproducts_main.php
+++ control/class.tx_ttproducts_main.php
@@ -843,7 +843,7 @@ class tx_ttproducts_main implements SingletonInterface
                 case 'SINGLEAD':
                     $catView = GeneralUtility::makeInstance('tx_ttproducts_cat_view');
                     $catView->init(
-                        $pibaseOb->getContentObjectRenderer(),
+                        $pibaseObj->getContentObjectRenderer(),
                         $this->pid,
                         $config['pid_list'],
                         $config['recursive']
