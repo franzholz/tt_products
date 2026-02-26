d245636ddca8ea68f67b9a012e03cb1ffe52505c
Franz Holzinger <franz.holzinger@festwein.de>
2026-02-26 18:30:35 +0100

    change tt_products_products_mm_articles treatment

--- Configuration/TCA/Overrides/tt_products_products_mm_articles.php
+++ Configuration/TCA/Overrides/tt_products_products_mm_articles.php
@@ -2,8 +2,41 @@
 
 defined('TYPO3') || die('Access denied.');
 
-call_user_func(function (): void {
-    $table = 'tt_products_products_mm_articles';
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToInsertRecords($table);
-});
+call_user_func(function ($extensionKey, $table): void {
+
+    $languageSubpath = '/Resources/Private/Language/';
+    $languageLglPath = 'LLL:EXT:core' . $languageSubpath . 'locallang_general.xlf:LGL.';
+
+    if (empty($GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$extensionKey]['articleMode'])) {
+        $GLOBALS['TCA'][$table] = [
+            'ctrl' => [
+                'title' => 'LLL:EXT:' . $extensionKey . $languageSubpath . 'locallang_db.xlf:tt_products_products_mm_articles',
+            'label' => 'uid_local',
+            'tstamp' => 'tstamp',
+            'crdate' => 'crdate',
+            'type' => 'uid_local:type',
+            'hideTable' => true,
+            'delete' => 'deleted',
+            'enablecolumns' => [
+                'disabled' => 'hidden',
+            ],
+            'crdate' => 'crdate',
+            'iconfile' => 'EXT:' . $extensionKey . '/Resources/Public/Icons/tt_products_relations.gif',
+            'hideTable' => true,
+            ],
+            // Article palette, hidden but needs to be included all the time
+            'articlePalette' => [
+                'showitem' => 'uid_local',
+            'isHiddenPalette' => true,
+            ],
+        ];
+    }
+
+    debug ('B');
+    debug ($GLOBALS['TCA'][$table], '$GLOBALS[\'TCA\']['.$table.'] HIER +++');
+    debug ('E');
+
+    ExtensionManagementUtility::addToInsertRecords($table);
+}, 'tt_products', basename(__FILE__, '.php'));
